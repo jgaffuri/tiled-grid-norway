1ab337eb38e6f781cc8d32a275e62fcf031a038a
jgaffuri <jgaf@proton.me>
2023-09-12 12:00:58 +0200

    a

--- src/pop_1000m.js
+++ src/pop_1000m.js
@@ -13,21 +13,31 @@ const prepareCSV = async (year) => {
     console.log("Load CSV file " + year)
 
     //load files
-    let dt = await aq.loadCSV("./input/pop_1000m/NOR01000M_POP_" + year + ".csv", { delimiter: ";" });
+    let dt = await aq.loadCSV("./input/pop_1000m/NOR1000M_POP_" + year + ".csv", { delimiter: ";" });
 
     //rename population column
     //dt = dt.rename({ "pop_tot": "y" + y })
 
-    console.log(dt.columnNames())
+    dt = dt.derive({
+        ta: aq.escape(d => +(d.pop_tot * +d.pop_ave2.replace(",", ".")).toFixed(1))
+    });
+
+    //keep only relevant columns
+    dt = dt.select("SSBID1000M", "pop_tot", "pop_mal", "pop_fem", "ta")
+
+    //rename columns
+    dt = dt.rename({ "pop_tot": "p", "pop_mal": "m", "pop_fem": "f" })
+
+    console.log(dt.print());
+
+    //console.log(dt.columnNames())
 
     console.log("Save")
-
-    const csv = aq.table(out).toCSV()
+    const csv = aq.table(dt).toCSV()
     writeFileSync("./input/out_pop_1000m_" + year + ".csv", csv);
-
 }
 
-prepareCSV(2019)
+//prepareCSV(2019)
 
 
 
@@ -35,15 +45,15 @@ prepareCSV(2019)
 const tiling = (year) => {
 
     //gothrough several aggregation levels
-    for (let a of [1, 2, 5, 10, 20, 50]) {
+    for (let a of [1, 2, 5, 10, 20, 50, 100]) {
 
         console.log("Tiling " + year + " to " + a + "m")
 
         execSync(
-            'gridtiler -i ./input/pop_1000m/NOR1000m.csv -r 250 -c 3035 -x 1900000 -y 6400000 -p "return {x:+c.SSBID0250M.substring(0,7), y:+c.SSBID0250M.substring(7,14)};" -m "delete c.SSBID0250M" -a ' + a + ' -o ./out/popcsv/' + (a * 250) + 'm/ -e csv -t 32'
+            'gridtiler -i ./input/out_pop_1000m_' + year + '.csv -r 1000 -c 25833 -x 1900000 -y 6400000 -p "return {x:+c.SSBID1000M.substring(0,7), y:+c.SSBID1000M.substring(7,14)};" -m "delete c.SSBID1000M" -a ' + a + ' -o ./out/pop'+year+'csv/' + (a * 1000) + 'm/ -e csv -t 128'
             , { stdio: 'inherit' }
         );
     }
 }
 
-//tiling()
+tiling(2019)

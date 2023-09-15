0e69cf8fd4a8ff05eafb8127049e13559dfba7b3
jgaffuri <jgaf@proton.me>
2023-09-15 16:05:08 +0200

    NO

--- src/pop_250m.js
+++ src/pop_250m.js
@@ -11,16 +11,10 @@ const { execSync } = require("child_process");
 const prepareCSV = async (year) => {
 
     console.log("Load CSV file " + year)
-
-    //load files
     let dt = await aq.loadCSV("./input/pop_250m/NOR0250M_POP_" + year + ".csv", { delimiter: ";" });
 
     //rename columns
     dt = dt.rename({ "pop_tot": "p" })
-
-    //console.log(dt.print());
-
-    //console.log(dt.columnNames())
 
     console.log("Save")
     const csv = aq.table(dt).toCSV()
@@ -28,7 +22,7 @@ const prepareCSV = async (year) => {
 }
 
 //prepare
-for (let y = 2001; y <= 2022; y++) prepareCSV(y)
+//for (let y = 2001; y <= 2022; y++) prepareCSV(y)
 
 
 
@@ -36,16 +30,16 @@ for (let y = 2001; y <= 2022; y++) prepareCSV(y)
 const tiling = (year) => {
 
     //gothrough several aggregation levels
-    for (let a of [1, 2, 5, 10, 20, 50, 100]) {
+    for (let a of year < 2020 ? [1, 2] : [1, 2, 4, 8, 20, 40, 80, 200, 400]) {
 
         console.log("Tiling " + year + " to " + a + "m")
 
         execSync(
-            'gridtiler -i ./input/out_pop_1000m_' + year + '.csv -r 1000 -c 25833 -x 1900000 -y 6400000 -p "return {x:+c.SSBID1000M.substring(0,7), y:+c.SSBID1000M.substring(7,14)};" -m "delete c.SSBID1000M" -a ' + a + ' -o ./out/pop' + year + 'csv/' + (a * 1000) + 'm/ -e csv -t 128'
+            'gridtiler -i ./input/out_pop_250m_' + year + '.csv -r 250 -c 25833 -x 1900000 -y 6400000 -p "return {x:+c.SSBID1000M.substring(0,7), y:+c.SSBID1000M.substring(7,14)};" -m "delete c.SSBID1000M" -a ' + a + ' -o ./out/pop' + year + 'csv/' + (a * 250) + 'm/ -e csv -t 128'
             , { stdio: 'inherit' }
         );
     }
 }
 
 //tiling
-//for (let y = 2001; y <= 2019; y++) tiling(y)
+for (let y = 2001; y <= 2022; y++) tiling(y)

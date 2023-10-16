3e4cf5f4f47886d26482c641487648949b8cfccc
jgaffuri <jgaf@proton.me>
2023-10-16 20:01:42 +0200

    roundings

--- src/pop_time_series_250m.js
+++ src/pop_time_series_250m.js
@@ -16,7 +16,7 @@ const prepareCSV = async () => {
     //load all files, one by one
     const tables = []
     for (let y = 2001; y <= 2022; y++) {
-        let dt = await aq.loadCSV("./input/NOR0250M_POP_" + y + ".csv", { delimiter: ";" });
+        let dt = await aq.loadCSV("./input/pop_250m/NOR0250M_POP_" + y + ".csv", { delimiter: ";" });
         //rename population column
         dt = dt.rename({ "pop_tot": "y" + y })
         tables.push(dt)
@@ -34,7 +34,7 @@ const prepareCSV = async () => {
     console.log("Save")
 
     const csv = aq.table(out).toCSV()
-    writeFileSync("./input/out.csv", csv);
+    writeFileSync("./input/out_pop_250m.csv", csv);
 
 }
 
